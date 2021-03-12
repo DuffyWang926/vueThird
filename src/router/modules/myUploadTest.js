68df494b62e6fbd5085097f5befa6790bb651621
赵翰宇-Finale <910149949@qq.com>
2021-03-12 16:49:16 +0800

    3-12 完成了MyUpload组件的开发和测试页面 赵翰宇

---
+++ src/router/modules/myUploadTest.js
@@ -0,0 +1,10 @@
+export default [
+  {
+    path: '/myUploadTest',
+    name: 'myUploadTest',
+    meta: {
+      index: 2
+    },
+    component: () => import('@/views/myUploadTest/index')
+  }
+]
---
+++ src/views/myUploadTest/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <my-upload
+    v-model:image-list="imageList"
+    :multiple="true"
+    :limit="3"
+  ></my-upload>
+</template>
+
+<script>
+import { ref } from 'vue'
+export default {
+  setup() {
+    const imageList = ref([])
+    return {
+      imageList
+    }
+  }
+}
+</script>
+
+<style>
+</style>
